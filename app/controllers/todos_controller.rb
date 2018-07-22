f584e9722c1bde24c408c0e5f9fc9666fffc05d0
fredrick onduso <freddysmallz@gmail.com>
2018-07-22 06:10:37 +0000

    first commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -35,6 +35,11 @@ class TodosController < ApplicationController
          end
      end
 
+     def index
+         @todos = Todo.all
+
+     end
+
     private
     def todo_params
         params.require(:todo).permit(:name, :description)
